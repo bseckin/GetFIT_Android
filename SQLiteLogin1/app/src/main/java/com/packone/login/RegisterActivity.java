9c0658b9ed0899a5e9ce469274c8c53894980be7
Berkay Seckin <bseckin@student.tgm.ac.at>
2014-10-06 12:36:15 +0200

    sydxfc
    sad

--- SQLiteLogin1/app/src/main/java/com/packone/login/RegisterActivity.java
+++ SQLiteLogin1/app/src/main/java/com/packone/login/RegisterActivity.java
@@ -19,7 +19,6 @@ public class RegisterActivity extends Activity {
     private EditText mpword;
     private EditText memail;
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
